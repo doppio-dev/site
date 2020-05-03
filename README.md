b1b1ad03c047cc5b86d7089654c055f2386aeb8a
Igor Kravchenko <gornivv@gmail.com>
2020-05-03 21:16:23 +0300

    update ci

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # site
+
 web site for doppio.dev
+
+NOT work with 1.18.0-8.0.pre - use masrer channel!
---
+++ _bump.sh
@@ -0,0 +1,17 @@
+echo '$1 = ' $1
+echo '$2 = ' $2
+newNumber=$(($2 + 100))
+echo $newNumber
+if  [[ $1 == release/* ]];
+then
+    echo 'yes'
+    newversion=$(echo $1 | sed "s|release\/\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)|\1\.\2\.\3+$newNumber|")
+else
+    echo 'no'
+    version=$(sed -n '18p' pubspec.yaml)
+    newversion=$(echo $version | sed "s|version: \([0-9]*\)\.\([0-9]*\)\.\([0-9]*\)\+\([0-9]*\)|\1\.\2\.\3+$newNumber|g")
+fi
+echo 'version: '$newversion
+sed -i '' 's|version: .*|version: '$newversion'|' pubspec.yaml
+# sed -i '' 's|appVersion = .*|appVersion = \"'$newversion'\";|g' lib/main.dart
+echo 'bumped version'
