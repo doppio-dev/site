d4a0dba475d8af78d3c8d1088a7ab7a2f1cd5a6e
Igor Kravchenko <gornivv@gmail.com>
2020-05-04 10:42:24 +0300

    change create-dmg

--- _dmg.sh
+++ _dmg.sh
@@ -1,25 +1,9 @@
 # test -f doppio_dev_site.dmg && rm doppio_dev_site.dmg
 rm -R release/macos
-cp -Rf "build/macos/Build/Products/Release/doppio_dev_site.app" "build/macos/Build/Products/Release/Doppio.dev Site.app"
+cp -r "build/macos/Build/Products/Release/doppio_dev_site.app" "build/macos/Build/Products/Release/Doppio.dev Site.app"
 echo "Start create-dmg"
-create-dmg \
-  --no-internet-enable \
-  --volname "Doppio.dev Site Installer" \
-  --volicon "logo/icon.icns" \
-  --window-pos 200 120 \
-  --window-size 800 529 \
-  --icon-size 130 \
-  --text-size 14 \
-  --icon "Doppio.dev Site.app" 260 250 \
-  --hide-extension "Doppio.dev Site.app" \
-  --app-drop-link 540 250 \
-  --hdiutil-quiet \
-  --sandbox-safe \
-  "doppio_dev_site.dmg" \
-  "build/macos/Build/Products/Release/Doppio.dev Site.app/"
-echo "Finish create-dmg"
-ls
 mkdir release
 mkdir release/macos
-cp -Rf doppio_dev_site.dmg release/macos/
-rm doppio_dev_site.dmg
+# https://github.com/sindresorhus/create-dmg
+create-dmg  'build/macos/Build/Products/Release/Doppio.dev Site.app' 'release/macos'
+echo "Finish create-dmg"
